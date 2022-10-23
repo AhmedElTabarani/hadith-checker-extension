aaa961e13bfe127041290b4178765d32d58efd24
أحمد الطبراني <eltabaraniahmed@gmail.com>
2022-10-23 15:24:22 +0200

    Revert "Share buttons (#9)"
    This reverts commit a6f2ce1b2ce63b07c1fc470d1dbb1d65cc696d5f.

--- popup.js
+++ popup.js
@@ -5,7 +5,6 @@ import {
 
 const cards = document.getElementsByClassName('cards')[0];
 const next = document.getElementById('next');
-const shareButton = document.querySelectorAll('#share-button');
 const prev = document.getElementById('prev');
 const pageCounter = document.querySelector('#page-counter span');
 const hadithCounter = document.querySelector('#hadith-counter span');
@@ -62,32 +61,13 @@ const updateContent = (allHadith) => {
                 <p class="hadith-number"><span>رقم الحديث أو الصفحة:</span> ${number_or_page}</p>
                 <p class="hadith-grade"><span>صحة الحديث:</span> ${grade}</p>
              </div>
-             <button id="share-button" data-hadith=${hadith}>
-             <svg aria-hidden="true" data-prefix="fal" data-icon="share-square" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="svg-inline--fa fa-share-square fa-w-18 fa-7x"><path fill="currentColor" d="M566.633 169.37L406.63 9.392C386.626-10.612 352 3.395 352 32.022v72.538C210.132 108.474 88 143.455 88 286.3c0 84.74 49.78 133.742 79.45 155.462 24.196 17.695 58.033-4.917 49.7-34.51C188.286 304.843 225.497 284.074 352 280.54V352c0 28.655 34.654 42.606 54.63 22.63l160.003-160c12.489-12.5 12.489-32.76 0-45.26zM384 352V248.04c-141.718.777-240.762 15.03-197.65 167.96C154.91 393 120 351.28 120 286.3c0-134.037 131.645-149.387 264-150.26V32l160 160-160 160zm37.095 52.186c2.216-1.582 4.298-3.323 6.735-5.584 7.68-7.128 20.17-1.692 20.17 8.787V464c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h172.146c6.612 0 11.954 5.412 11.852 12.04-.084 5.446-4.045 10.087-9.331 11.396-9.462 2.343-18.465 4.974-27.074 7.914-1.25.427-2.555.65-3.876.65H48c-8.837 0-16 7.163-16 16v352c0 8.837 7.163 16 16 16h352c8.837 0 16-7.163 16-16v-50.002c0-3.905 1.916-7.543 5.095-9.812z" class=""></path></svg>
-             <button>
         </div>`;
   });
 
   cards.innerHTML = allCardsDiv.join('');
   hideLoader();
 };
-const shareIt = (hadith) => {
-  if (hadith) {
-    navigator.share({
-      title: 'Hadith checker',
-      text: hadith,
-      url: './icons/icons128.png',
-    });
-  }
-};
-// if navigator.share supported then add event listener  on click to all share butons instances
-// if navigator.share is not supported then add style.display none to all share button instances
-shareButton.forEach((box) => {
-  if (!navigator.share) {
-    box.style.display = 'none';
-  }
-  box.addEventListener('click', () => shareIt(box.dataset.hadith));
-});
+
 const updatePageCounter = () => {
   pageCounter.innerText = currPage;
 };
