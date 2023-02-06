607f0a4e14415de80b641a4816e1671de8f10c35
ahmedsa11 <ahmedsa122333@gmail.com>
2023-02-06 23:41:59 +0200

    add box

---
+++ showbox.js
@@ -0,0 +1,7 @@
+//show
+document.querySelector(".toggle-settings .fa-gear").onclick = function () {
+    this.classList.toggle("fa-spin");
+    document.querySelector(".settings-box").classList.toggle("open");
+    
+  };
+  
