ec9b961bc37f94243a9580ff7f700504a92b7b7a
AhmedEl-Tabarani <eltabaraniahmed@gmail.com>
2023-03-29 02:13:22 +0200

    delete some comments, script and link tags

--- search-window.js
+++ search-window.js
@@ -1,20 +1,11 @@
-//for search window
-// document.querySelector(
-//   '.searchwindow .toggle-settings .fa-gear',
-// ).onclick = function () {
-//   chrome.windows.create({
-//     url: chrome.runtime.getURL('popup.html'),
-//     type: 'popup',
-//   });
-// };
-//search window
+// search window
 const searchbar = document.querySelector('#searchbar');
 const btnSearch = document.querySelector('#search-window-btn');
 btnSearch.addEventListener('click', () => {
-    if(searchbar.value===''){
-        document.querySelector('.err').innerHTML="هذا الحقل مطلوب"
-        return false;
-    }
+  if (searchbar.value === '') {
+    document.querySelector('.err').innerHTML = 'هذا الحقل مطلوب';
+    return;
+  }
   chrome.storage.local.set({ text: searchbar.value }, async () => {
     await chrome.windows.create({
       url: chrome.runtime.getURL('popup.html'),
