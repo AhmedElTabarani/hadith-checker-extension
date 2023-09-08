7f625440ce63a8b845e03c711d5b2b89da3171db
AhmedEl-Tabarani <eltabaraniahmed@gmail.com>
2023-09-08 16:05:58 +0200

    Trigger Enter key

--- public/js/search-window.js
+++ public/js/search-window.js
@@ -2,6 +2,14 @@ import * as cache from '../../utils/cache.js';
 
 const searchbar = document.querySelector('#searchbar');
 const btnSearch = document.querySelector('#search-window-btn');
+
+searchbar.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    btnSearch.click();
+  }
+});
+
 btnSearch.addEventListener('click', async () => {
   if (searchbar.value === '') {
     document.querySelector('.err').innerHTML = 'هذا الحقل مطلوب';
